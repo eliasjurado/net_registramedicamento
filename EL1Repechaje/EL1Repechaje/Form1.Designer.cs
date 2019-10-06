a3d7fc9b77aa994f9846b4619bf54ab5af4874f4
Elías Luis Jurado Santos <eliasljurado@gmail.com>
2019-10-05 22:33:25 -0500

    Carga de Archivos
    No debería pasar 1A2B3C pero pasa.

---
+++ EL1Repechaje/EL1Repechaje/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace EL1Repechaje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.cboEst = new System.Windows.Forms.ComboBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(151, 25);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(189, 20);
+            this.txtCod.TabIndex = 0;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(151, 65);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(189, 20);
+            this.txtNom.TabIndex = 0;
+            // 
+            // cboEst
+            // 
+            this.cboEst.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEst.FormattingEnabled = true;
+            this.cboEst.Items.AddRange(new object[] {
+            "vigente",
+            "vencido"});
+            this.cboEst.Location = new System.Drawing.Point(151, 109);
+            this.cboEst.Name = "cboEst";
+            this.cboEst.Size = new System.Drawing.Size(189, 21);
+            this.cboEst.TabIndex = 1;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(114, 163);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(132, 23);
+            this.btnRegistrar.TabIndex = 2;
+            this.btnRegistrar.Text = "Registrar Medicamento";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "código del medicamento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "nombre del medicamento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "estado de medicamento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 210);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.cboEst);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtCod);
+            this.Name = "Form1";
+            this.Text = "Registro de Medicamento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.ComboBox cboEst;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EL1Repechaje/EL1Repechaje/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace EL1Repechaje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtCod.Text, @"^[5-9]\d{5}$"))
+            {
+                MessageBox.Show("Ingrese un código válido");
+            }
+            //Regex para nombre de medicamento, incluye: numeros reales, porcentajes, grado del alcohol, logos registrados  
+            else if (!Regex.IsMatch(txtNom.Text, @"^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜñÑçÇ0-9\s\-\/\+\±\(\)\,\.\:\;\·\%\""\#\*\°\'\&\=\>\<\@\©\®\™\¼\¾\½\÷\‰]{4,}$"))
+            {
+                MessageBox.Show("Recuerde que el nombre de medicamento debe tener al menos 4 caracteres");
+            }
+            else if (cboEst.SelectedIndex < 0)
+            {
+                MessageBox.Show("Debe seleccionar un estado de producto válido");
+            }
+            else
+            {
+                MessageBox.Show("El registro ha sido exitoso");
+            }
+
+        }
+    }
+}
